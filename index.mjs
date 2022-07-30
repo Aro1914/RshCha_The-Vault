bdabda2397f3fc6cefcad6cd9b52dd9fbe0c5eb0
Your NameEmmanuel <agbavweisaac@gmail.com>
2022-07-30 17:26:19 +0100

    Update level 1 (story version) completed

--- index.mjs
+++ index.mjs
@@ -1,27 +1,66 @@
-import {loadStdlib} from '@reach-sh/stdlib';
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(6000));
+const accBob = await stdlib.newTestAccount(startingBalance);
+console.log('The Story of The Vault!');
 
 console.log('Launching...');
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+const getBalance = async (who) => stdlib.formatCurrency((await stdlib.balanceOf(who)));
+
+console.log(`Alice's initial balance is: ${await getBalance(accAlice)}`);
+console.log(`Bob's initial balance is: ${await getBalance(accBob)}`);
+
+const sharedInterface = () => ({
+  showDeadline: (deadline) => {
+    console.log(parseInt(deadline));
+  }
+});
+
+const declareQuest = () => {
+  console.log(`Alice announces a new quest - The Vault with a bounty of 5000 to whoever completes it`);
+  return stdlib.parseCurrency(5000);
+};
 console.log('Starting backends...');
 await Promise.all([
   backend.Alice(ctcAlice, {
     ...stdlib.hasRandom,
+    ...sharedInterface(),
+    bounty: declareQuest(),
+    decide: () => {
+      const decision = Math.floor(Math.random() * 2);
+      const statement = [`fulfill the terms of the quest`, `go back on her words`];
+      console.log(`Alice decided to ${statement[decision]}`);
+      return decision ? true : false;
+    }
     // implement Alice's interact object here
   }),
   backend.Bob(ctcBob, {
     ...stdlib.hasRandom,
+    ...sharedInterface(),
     // implement Bob's interact object here
+    acceptQuest: (price) => {
+      console.log(`Bob accepts the terms of The Vault quest for ${stdlib.formatCurrency(price)}`);
+      console.log(`Bob completes the quest valiantly`);
+      return true;
+    },
+    showStatus: (aliceDecision) => {
+      if (!aliceDecision) {
+        console.log(`Bob rejoices`);
+      } else {
+        console.log(`Poor Bob`);
+      }
+    }
   }),
 ]);
 
-console.log('Goodbye, Alice and Bob!');
+console.log(`Alice's final balance is: ${await getBalance(accAlice)}`);
+console.log(`Bob's final balance is: ${await getBalance(accBob)}`);
+
+console.log('The End!');
--- index.rsh
+++ index.rsh
@@ -1,19 +1,57 @@
 'reach 0.1';
 
+const [DEADLINE, sharedInterface] = [20, {
+  showDeadline: Fun([UInt], Null)
+}];
+
 export const main = Reach.App(() => {
   const A = Participant('Alice', {
     // Specify Alice's interact interface here
+    ...sharedInterface,
+    decide: Fun([], Bool),
+    bounty: UInt,
   });
   const B = Participant('Bob', {
     // Specify Bob's interact interface here
+    ...sharedInterface,
+    acceptQuest: Fun([UInt], Bool),
+    showStatus: Fun([Bool], Null),
   });
   init();
   // The first one to publish deploys the contract
-  A.publish();
+  A.only(() => {
+    const bounty = declassify(interact.bounty);
+  });
+  A.publish(bounty).pay(bounty);
   commit();
   // The second one to publish always attaches
-  B.publish();
+  B.only(() => {
+    const consent = declassify(interact.acceptQuest(bounty));
+  });
+  B.publish(consent);
   commit();
+
+  each([A, B], () => {
+    interact.showDeadline(DEADLINE);
+  });
+
+  A.only(() => {
+    const decision = declassify(interact.decide());
+  });
+  A.publish(decision);
+
+  if (decision) {
+    transfer(bounty).to(A);
+    B.only(() => {
+      interact.showStatus(decision);
+    });
+  } else {
+    transfer(bounty).to(B);
+    B.only(() => {
+      interact.showStatus(decision);
+    });
+  }
   // write your program here
+  commit();
   exit();
 });
